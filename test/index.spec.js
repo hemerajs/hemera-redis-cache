ef535506596f37a95753c24928ef996a16f62b2d
ggn06awu <mark.beeby@gmail.com>
2018-04-09 10:42:22 +0100

    Unit test for KEYS and cleanup for existing tests
    This adds a test for the a Redis KEYS operation, and adds a cleanup to the existing GET and SET tests.

--- test/index.spec.js
+++ test/index.spec.js
@@ -38,32 +38,77 @@ describe('Hemera-redis-cache', function() {
     done()
   })
 
-  it('set', function() {
-    return hemera
-      .act({
-        topic: 'redis-cache',
-        cmd: 'set',
-        key: 'example',
-        value: 100
-      })
-      .then(resp => expect(resp.data).to.be.equals('OK'))
+  describe('SET operation', () => {
+    it('set', function() {
+      return hemera
+          .act({
+            topic: 'redis-cache',
+            cmd: 'set',
+            key: 'example',
+            value: 100
+          })
+          .then(resp => expect(resp.data).to.be.equals('OK'))
+    })
+
+    after(function(done) {
+      client.del('example', done)
+    })
   })
 
-  it('get', function() {
-    return hemera
-      .act({
-        topic: 'redis-cache',
-        cmd: 'set',
-        key: 'example',
-        value: 100
-      })
-      .then(() => {
-        return hemera.act({
+  describe('GET operation', () => {
+    it('get', function() {
+      return hemera
+        .act({
           topic: 'redis-cache',
-          cmd: 'get',
-          key: 'example'
+          cmd: 'set',
+          key: 'example',
+          value: 100
         })
-      })
-      .then(resp => expect(resp.data).to.be.equals('100'))
+        .then(() => {
+          return hemera.act({
+            topic: 'redis-cache',
+            cmd: 'get',
+            key: 'example'
+          })
+        })
+        .then(resp => expect(resp.data).to.be.equals('100'))
+    })
+
+    after(function(done) {
+      client.del('example', done)
+    })
+  })
+
+  describe('KEYS operation', () => {
+    it('keys', function() {
+      return hemera
+        .act({
+          topic: 'redis-cache',
+          cmd: 'set',
+          key: 'example',
+          value: 100
+        })
+        .then(() => {
+          hemera
+            .act({
+              topic: 'redis-cache',
+              cmd: 'set',
+              key: 'example2',
+              value: 200
+            })
+        })
+        .then(() => {
+          return hemera.act({
+            topic: 'redis-cache',
+            cmd: 'keys',
+            pattern: 'examp*'
+          })
+        })
+        .then(resp => expect(resp.data.sort()).to.be.equals(['example', 'example2']))
+    })
+
+    after(function(done) {
+      client.del('example example2', done)
+    })
   })
 })
